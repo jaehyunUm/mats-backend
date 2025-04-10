c21d8d19bcabc571ec69a4cdbd8eb2d94f489ffe
엄재현 <jaehyunum@eomjaehcBookAir.lan>
2025-04-10 17:49:48 -0400

    Update backend code

--- schedulers/notificationScheduler.js
+++ schedulers/notificationScheduler.js
@@ -1,7 +1,6 @@
 const cron = require('node-cron');
 const { checkPayInFullNotifications } = require('./checkNotifications');
 
-// 새 코드: 1분마다 실행
 cron.schedule('* * * * *', () => {
   console.log("📆 Running Pay In Full notification check every minute...");
   checkPayInFullNotifications();
