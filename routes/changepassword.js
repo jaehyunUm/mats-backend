b7bb851229e60eaf4c42b954ffaa25d9326e99b6
엄재현 <jaehyunum@MacBookAir.lan>
2025-08-05 21:18:37 -0400

    Update backend code

--- routes/changepassword.js
+++ routes/changepassword.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db');
 const verifyToken = require('../middleware/verifyToken');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 // POST /api/change-password
 router.post('/change-password', verifyToken, async (req, res) => {
